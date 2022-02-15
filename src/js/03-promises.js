f18dfc6f045ea2a383fb7972b4a9bfc0507d3209
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-15 17:31:54 +0200

    +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+// Описан в документации
+import flatpickr from "flatpickr";
+// Дополнительный импорт стилей
+import "flatpickr/dist/flatpickr.min.css";
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
