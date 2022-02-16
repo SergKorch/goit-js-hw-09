5b3fd268446663d6ac3821cf6c83edd334e7e071
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-16 12:19:08 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,14 +6,20 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 let stopBg;
-function backGrColor() {
+function backGrColorStart() {
   stopBg = setInterval(() => {
     backGr.style.backgroundColor = getRandomHexColor();
   }, 1000);
+  startBtn.removeEventListener('click', backGrColorStart);
+  startBtn.disabled = 'true';
+  stopBtn.removeAttribute('disabled');
 }
 function backGrColorStop() {
   clearInterval(stopBg);
+  stopBtn.removeEventListener('click', backGrColorStop);
+  startBtn.removeAttribute('disabled');
+  stopBtn.disabled = 'true';
 }
-
-startBtn.addEventListener('click', backGrColor);
+stopBtn.disabled = 'true';
+startBtn.addEventListener('click', backGrColorStart);
 stopBtn.addEventListener('click', backGrColorStop);
