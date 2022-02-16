d4dd105aeae3bf500d32d2b66df9706a9c129c17
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-16 12:57:04 +0200

    +

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,24 @@
 // all modules
 import Notiflix from 'notiflix';
-
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 // one by one
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { Block } from 'notiflix/build/notiflix-block-aio';
+
+// Дополнительный импорт стилей
+flatpickr(selector, options)
+const elInputDate=document.querySelector('#datetime-picker')
+console.log(elInputDate)
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,1 @@
-// Описан в документации
-import flatpickr from "flatpickr";
-// Дополнительный импорт стилей
-import "flatpickr/dist/flatpickr.min.css";
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
