3293914ac82789fdb7d5a2a3d857a9193727eec9
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-16 17:38:25 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,25 +1,83 @@
-// all modules
 import Notiflix from 'notiflix';
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-// one by one
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { Report } from 'notiflix/build/notiflix-report-aio';
-// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
-// import { Loading } from 'notiflix/build/notiflix-loading-aio';
-// import { Block } from 'notiflix/build/notiflix-block-aio';
-
-// Дополнительный импорт стилей
-
-
-const elInputDate=document.querySelector('#datetime-picker')
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  elInputDate: document.querySelector('input[type="text"]'),
+  clockFace: document.querySelector('.timer'),
+};
+console.log(refs.clockFace);
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-  flatpickr(elInputDate, options)
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr(refs.elInputDate, options);
+// const futureDate = new Date(elInputDate.value);
+class Timer  {
+  constructor({onTick}){
+    this.intervalId=null;
+    this.isActive=false;
+    this.onTick=onTick;
+    this.init();
+  }
+  init(){
+    const timeForm = this.convertMs(0);
+    this.onTick(timeForm)
+  }
+    start(){
+    if (this.isActive){
+return
+    }
+    const startTime = Date.now();
+    this.isActive=true
+    this.intervalId=setInterval(() => {
+      const currentTime = Date.now();
+      const dataTime = currentTime - startTime;
+      const timeForm = this.convertMs(dataTime);
+      console.log(timeForm);
+      // timerFace(timeForm)
+      this.onTick(timeForm)
+    }, 1000);
+  }
+  // stop(){
+  //   this.isActive=false
+  // },
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = this.pad(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.pad(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.pad(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = this.pad(Math.floor((((ms % day) % hour) % minute) / second));
+  
+    return { days, hours, minutes, seconds };
+  }
+  pad(element) {
+    return String(element).padStart(2, '0');
+  }
+};
+const timer = new Timer({
+  onTick : timerFace,
+})
+
+refs.startBtn.addEventListener('click', () => {
+  timer.start();
+});
+
+ 
+function timerFace({days,hours,minutes, second}) {
+  refs.clockFace.textContent = '${days}:${hours}:${minutes}:${seconds}';
+}
