d9b11427354c0746e6e9049fedbe3a7da73ff96a
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-16 13:12:52 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,16 +3,16 @@ import Notiflix from 'notiflix';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 // one by one
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Report } from 'notiflix/build/notiflix-report-aio';
-import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { Block } from 'notiflix/build/notiflix-block-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Report } from 'notiflix/build/notiflix-report-aio';
+// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
+// import { Block } from 'notiflix/build/notiflix-block-aio';
 
 // Дополнительный импорт стилей
-flatpickr(selector, options)
+
+
 const elInputDate=document.querySelector('#datetime-picker')
-console.log(elInputDate)
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -22,3 +22,4 @@ const options = {
       console.log(selectedDates[0]);
     },
   };
+  flatpickr(elInputDate, options)
