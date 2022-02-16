5cc2e22718a5591df339c9332c5f3f13c5cc9c0b
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-16 11:57:27 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const backGr = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let stopBg;
+function backGrColor() {
+  stopBg = setInterval(() => {
+    backGr.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function backGrColorStop() {
+  clearInterval(stopBg);
+}
+
+startBtn.addEventListener('click', backGrColor);
+stopBtn.addEventListener('click', backGrColorStop);
